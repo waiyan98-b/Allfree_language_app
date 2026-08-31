0fe9dbb9623ea360d7c8c152b2626c98cad2e03e
waiyan98-b <waiyankyawthu1998@gmail.com>
2026-08-31 15:29:57 +0700

    Create asset_paths.dart

---
+++ lib/core/constants/asset_paths.dart
@@ -0,0 +1,19 @@
+class AssetPaths {
+  AssetPaths._();
+
+  // Directories
+  static const String imagesDir = 'assets/images/';
+  static const String audioDir = 'assets/audio/';
+  static const String thaiAudioDir = 'assets/audio/thai/';
+  static const String fontsDir = 'assets/fonts/';
+
+  // UI Images
+  static const String logo = '${imagesDir}app_logo.png';
+  static const String thaiFlag = '${imagesDir}flag_thailand.png';
+  static const String myanmarFlag = '${imagesDir}flag_myanmar.png';
+
+  // Sound Effects
+  static const String soundCorrect = '${audioDir}correct.mp3';
+  static const String soundIncorrect = '${audioDir}incorrect.mp3';
+  static const String soundComplete = '${audioDir}complete.mp3';
+}
